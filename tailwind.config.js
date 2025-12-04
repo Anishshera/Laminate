625b466e41cc574e0d5ce85fd80d40910af1612e
Anishshera <103483835+Anishshera@users.noreply.github.com>
2025-12-04 14:27:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',  // lib, utils, etc sab cover ho jayega
   ],
   theme: {
     extend: {},
