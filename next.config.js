289e73c1b9929c5e7ee3e55b2032d99413069f8d
Anishshera <103483835+Anishshera@users.noreply.github.com>
2025-12-04 14:52:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // React strict mode on
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: '**', // sabhi domains allow
       },
     ],
+  },
+  typescript: {
+    ignoreBuildErrors: false, // agar TypeScript me error hua to build fail
   },
 };
 
